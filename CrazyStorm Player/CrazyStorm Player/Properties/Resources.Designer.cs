4e6b9005cef97849c8000df188ef642674ab3aa8
starx <zhuzhuoyue@gmail.com>
2026-02-26 11:21:59 +0800

    Fix dotnet build error

--- CrazyStorm Player/CrazyStorm Player/Properties/Resources.Designer.cs
+++ CrazyStorm Player/CrazyStorm Player/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace CrazyStorm_Player.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   查找类似于 (图标) 的 System.Drawing.Icon 类型的本地化资源。
-        /// </summary>
-        internal static System.Drawing.Icon logo {
-            get {
-                object obj = ResourceManager.GetObject("logo", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
